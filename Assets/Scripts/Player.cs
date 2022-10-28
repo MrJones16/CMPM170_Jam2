62e0da6982a98be674c588ec509819c15514aea0
Ben Paulsen <bjhpaulsen@gmail.com>
2022-10-27 17:57:29 -0700

    implement left right movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,6 +10,14 @@ public class Player : MonoBehaviour
     [Header("Settings")]
     [SerializeField]
     private float transitionDuration;
+    [SerializeField]
+    private float walkDuration;
+    [SerializeField]
+    private float forwardWalkDistance;
+    [SerializeField]
+    private float sidewaysWalkDistance;
+    [SerializeField]
+    private float walkTurnAngle;
 
     // states
     private bool eventing = true;
@@ -67,6 +75,7 @@ public class Player : MonoBehaviour
         float timeElapsed = 0;
         while(timeElapsed < transitionDuration)
         {
+            // change angle in an eased curve
             float t = timeElapsed / transitionDuration;
             t = t * t * (3f - 2f * t);
             float newAngle = toDivine ? Mathf.Lerp(0, 30, t) : Mathf.Lerp(30, 0, t);
@@ -91,14 +100,26 @@ public class Player : MonoBehaviour
         walking = true;
 
         float timeElapsed = 0;
-        while(timeElapsed < transitionDuration)
+        while(timeElapsed < walkDuration)
         {
-            float t = timeElapsed / transitionDuration;
-            t = t * t * (3f - 2f * t);
-            // float newAngle = right ? Mathf.Lerp(0, 30, t) : Mathf.Lerp(30, 0, t);
-            float newZ = Mathf.Lerp(0, 10, t);
-
+            // forward travel in a sine curve
+            float t = timeElapsed / walkDuration;
+            t = (Mathf.Sin(Mathf.PI*t-Mathf.PI/2)/2)+.5f;
+            float newZ = Mathf.Lerp(0, forwardWalkDistance, t);
             transform.position = new Vector3(transform.position.x, transform.position.y, newZ);
+
+            // sideways travel in a sine curve
+            float t2 = timeElapsed / walkDuration;
+            t2 = (Mathf.Sin(Mathf.PI*t2-Mathf.PI/2)/2)+.5f;
+            float newX = right? Mathf.Lerp(0, sidewaysWalkDistance, t) : Mathf.Lerp(0, -sidewaysWalkDistance, t);
+            transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+
+            // view angle follows a sine wave
+            float t3 = timeElapsed / walkDuration;
+            t3 = (Mathf.Sin(2*Mathf.PI*t3-Mathf.PI/2)/2)+.5f;
+            float newAngle = right ? Mathf.Lerp(0, walkTurnAngle, t3) : Mathf.Lerp(0, -walkTurnAngle, t3);
+            Vector3 angle = camera.transform.eulerAngles;
+            camera.transform.eulerAngles = new Vector3(angle.x, newAngle, angle.z);
             
             timeElapsed += Time.deltaTime;
             yield return null;
