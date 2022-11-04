478f007ffd8adafe7541103230874c6af069b6a2
Jose <joavelar@ucsc.edu>
2022-11-03 20:50:17 -0700

    Scenes Connected
    Go between scenes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
@@ -309,6 +310,7 @@ public class Player : MonoBehaviour
         foodAmount += sustenance;
         if (foodAmount <= 0) {
             //should end game
+            SceneManager.LoadScene(1);
             Debug.Log("GameOver");
         }
     }
@@ -320,6 +322,7 @@ public class Player : MonoBehaviour
         // escape the forest
         if (progress >= victoryCondition) {
             //should end game in a victory
+            SceneManager.LoadScene(2);
             Debug.Log("Victory");
         }
     }
